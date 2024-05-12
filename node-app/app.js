335fe551067cef2a2caa33c82b8b5f0821dbbd35
arvind-itt <arvind.menariya@intimetec.com>
2024-05-12 14:24:38 +0530

    added arvind in app.js file

--- node-app/app.js
+++ node-app/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Define a route handler for the root path
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.send('Hello, World Arvind!');
 });
 
 // Set the port to listen for incoming requests
